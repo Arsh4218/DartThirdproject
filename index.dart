9cc04cd44bf5dadb6b0bf0133305bd3e1efc32dd
Arsh4218 <122145610+Arsh4218@users.noreply.github.com>
2024-03-12 10:29:52 +0500

    Add files via upload

--- index.dart
+++ index.dart
@@ -8,6 +8,12 @@ var g = functionss(4, 4);
 print(g);
 var exp=Expression(6, 2);
 print(exp);
+
+//OPtional Functions
+optionalfunction(10);
+numbers(10,2,30);
+threenumber(3,z:4,y:4);
+
 }
 
 function(){
@@ -30,3 +36,18 @@ return
 }
 
 Expression(int a ,int b)=> (a*b)+10;
+
+
+
+//OPTIONAL FUNCTIONS
+optionalfunction(int a ,[int b=10]){
+    print('optional Parameter is override ${a+b}');
+}
+numbers(int x ,int y,int z){
+    var num= (x+y)*z;
+    print(num);
+}
+threenumber(int x ,{int y =0 ,int z= 0}){
+    var num =(x+y)*z;
+    print('Answer $num');
+}
