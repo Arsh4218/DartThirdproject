36eac6e372d5e22140ab03a9036661ac9c149812
Arsh4218 <122145610+Arsh4218@users.noreply.github.com>
2024-03-12 10:14:57 +0500

    Add files via upload

---
+++ index.dart
@@ -0,0 +1,32 @@
+main(){
+
+function();
+functions(10, 10);
+var formula = functionreturn(1, 4);
+print(formula);
+var g = functionss(4, 4);
+print(g);
+var exp=Expression(6, 2);
+print(exp);
+}
+
+function(){
+    print("welcome");
+}
+
+functions(int a ,int b){
+    print(a);
+    print(b);
+    print('Sum $a+$b');
+}
+functionreturn(int a, int b){
+    var x=(a*b)+10;
+    return x;
+}
+functionss (int a, int b){
+
+return
+(a*b);
+}
+
+Expression(int a ,int b)=> (a*b)+10;
