d477707d68be6486a883713cc0178b44f6501022
Arsh4218 <122145610+Arsh4218@users.noreply.github.com>
2024-03-21 09:45:31 -0700

    Add files via upload

---
+++ map.dart
@@ -0,0 +1,32 @@
+//MAP
+//Map is used to store difffernt multiple type of data,Maps are dictonary like data types that exist in key value form,
+//keys needs to be unique 
+// main(){
+//   var map_name ={
+// 'key1':'value1',
+
+// 'key2':'value2',
+// 'key3': '3.9',
+// 'key4': true
+//   };
+
+// map_name['key1'] ='qalbe';
+
+//   print(map_name);
+// }
+
+
+main(){
+
+  var mapName= Map();
+  mapName['Name']='Densel';
+  mapName['Year']=2022;
+
+  mapName['rollno']=36425;
+
+  mapName['city']='khi';
+  mapName['conditoin']= false;
+
+  
+  print(mapName);
+}
